c8aab57d8411ea3a8077ff6097b13388203c31a4
Polina Khafizova <polina.hafizova04@mail.ru>
2026-02-28 19:00:49 +0300

    шаг 1. завершен

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -12,16 +12,26 @@ import {
   selectFeedError
 } from '../../services/slices/feed-slice';
 
+import {
+  fetchIngredients,
+  selectIngredients
+} from '../../services/slices/ingredients-slice';
+
 export const Feed: FC = () => {
   const dispatch = useDispatch();
 
   const orders: TOrder[] = useSelector(selectFeedOrders);
   const isLoading = useSelector(selectFeedLoading);
   const error = useSelector(selectFeedError);
+  const ingredients = useSelector(selectIngredients);
 
   useEffect(() => {
+    if (!ingredients.length) {
+      dispatch(fetchIngredients());
+    }
+
     dispatch(fetchFeed());
-  }, [dispatch]);
+  }, [dispatch, ingredients.length]);
 
   if (isLoading) {
     return <Preloader />;
